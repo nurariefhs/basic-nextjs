bf0492d0014f00ac7796ef5ee52a9abf2a9e71cb
Nur Arief <arief.visione@gmail.com>
2021-11-05 14:38:47 +0700

    [ADD] Tailwind Integration

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Welcome</h1>
+      <h1 className="text-blue-700">Welcome</h1>
       
     </div>
   )
